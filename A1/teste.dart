24873e82443bc4d82f7a8bc2009d666b26939367
Moisés Camargo Da Silva <133894628+Moita12@users.noreply.github.com>
2024-02-21 13:04:25 -0300

    Add files via upload

---
+++ A1/teste.dart
@@ -0,0 +1,14 @@
+void main() {
+  String principal = 'main';
+  String nomeCombinado = 'José' + ' ' + 'Maria';
+  String nomeCombinadosMais = 'José' ' ' ' Maria';
+  String EnderecoMultilinhas = ''''
+  Itapetininga, Sp, 1820000000
+  Cidade, Estado, Cep
+  ''';
+
+  print(principal);
+  print(nomeCombinado);
+  print(nomeCombinadosMais);
+  print(EnderecoMultilinhas);
+}
---
+++ A1/teste3.dart
@@ -0,0 +1,10 @@
+void main() {
+  var num = 20;
+
+  //Operador Ternário
+  var mensagem =
+      num % 2 == 0 ? 'O número $num é par.' : 'O número $num é ímpar.';
+
+  //Imprimir a Mensagem
+  print(mensagem);
+}
