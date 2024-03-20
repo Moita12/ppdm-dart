bb3208ef3c960f866a33294b3afc459ef86b6ba3
Moisés Camargo Da Silva <133894628+Moita12@users.noreply.github.com>
2024-03-20 13:23:31 -0300

    Add files via upload

---
+++ A7/classes.dart
@@ -0,0 +1,16 @@
+void main(){
+    class carro(){
+    String placa;
+    int numChassi;
+
+    class animal(){
+        float tamanho;
+        String cor;
+    }
+    class consulta(){
+        date data;
+        String nomePaciente;
+        String nomeDentista;
+    }
+    }
+}
---
+++ A7/construtores.dart
@@ -0,0 +1,15 @@
+import 'classes.dart';
+void main(){
+    carro2 carro = new carro()
+
+    carro.placa = "EYW2741";
+    carro.numCHassi = 17
+
+    animal animal = new animal()
+        animal.tamanho = 67.9
+        animal.cor = "Amarelo"
+    
+    Consulta consulta = consulta()
+    consulta.nomePaciente = "Junior"
+    consulta.nomeDentista = "Douglas"
+}|
